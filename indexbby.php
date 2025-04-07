9160ab5db582648e88c53cfdda21631f9d357642
Most_Ashamed_Fingers_IRL_RN <mackan.maci@gmail.com>
2025-04-07 12:01:01 -0400

    I went & spent a few weeks revamping the whole ting locally on main branch j to have it all reset under my nose by by own fat fingers. What cursed fingers these are... Go make git protocol comic art like the warning signs in chinese factories that warn of the danger of hanging from above a bloody furnace. Let this be a warning to future self. Git is ur fwiend when used right... You are your own worst enemy when using git wrong

---
+++ indexbby.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html land="en">
+
+<head>
+<meta name="Quantum Markets" content="Quantum Markets LTD - ‘’Tailor-made Brokerage & Shipping – Trading – Operational
+Support for our Clients in Oil Trading & Tanker Industry’">
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="/css/stylesheet.css">
+    <link rel="stylesheet" href="/css/utilsStylesheet.css">
+    <link rel="stylesheet" href="/css/blockStylesheet.css">
+    <link rel="stylesheet" href="/css/buttonsStylesheet.css">
+
+    <script src="/node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="/javaScript/autoLoader.js"> </script>
+</head>
+<link rel="shortcut icon" href="https://quantum-mar-images.fra1.cdn.digitaloceanspaces.com/resources/images/logo_large.png" type="image/x-icon">
+<title>Quantum Markets</title>
+
+
+<body>
+    <!-- the navigation bar from essentials.html -->
+    <div class="navBar" id="NavBar">
+        
+    </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<!-- --------------------------------------------------------------------------------------------------- -->
+<div class="video-container">
+<body><div></div><div></div><div></div>
+    <div>
+        <div class="video-container">
+        <div class="contentBoxTransparent">
+        <video class="background-video" autoplay muted loop playsinline>
+        <source src="https://quantum-mar-images.fra1.cdn.digitaloceanspaces.com/resources/vids/tanker-roll.webm" type="video/webm">
+                Your browser does not support the video tag.
+        <p class="accentColorY titleLarge textAlignCenter">
+            QUANTUM MARKETS
+        </p>
+    </div>
+        </video>
+</div>
+    
+    
+</div>
+
+
+
+</body>
+
+<!-- ---------------------------------------------------------------------------------------------------- -->
+<!-- footer bar from essentials.html -->
+<footer>
+    <div class="socialsFooter">
+        <div class="socialsFooter" id="Footer"></div>
+    </div>
+</footer>
+
+
+</html>
